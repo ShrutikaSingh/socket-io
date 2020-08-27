d53d58334be63c6d0eec1868fc4ab7fc83281443
ShrutikaSingh <shrutika051220@gmail.com>
2020-08-27 14:23:22 +0530

    setup and refactor initial code base

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var path = require('path');
+const path=require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var http = require('http');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,5 +16,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+var server = http.createServer(app);
+var port = (process.env.PORT || '3000');
+
+
+public = path.join(__dirname, "/../public")
+
+server.listen(port,()=>{
+    console.log('here')
+});
 
 module.exports = app;
