42bd7023c1da4a2c420e1f26b5b170f96c8c88c3
ShrutikaSingh <shrutika051220@gmail.com>
2020-08-27 15:21:03 +0530

    const socketIO = reuire ('socket.io');

--- server/app.js
+++ server/app.js
@@ -4,11 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 const http = require('http');
+const socketIO = reuire ('socket.io');
 var app = express();
-let server=http.createServer(app);
-
 publicpath = path.join(__dirname, "/../public")
 
 app.use(logger('dev'));
@@ -19,6 +17,9 @@ app.use(express.static(publicpath));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+let server=http.createServer(app);
+let io=soc
 
 var port = (process.env.PORT || '3000');
 server.listen(port,()=>{
